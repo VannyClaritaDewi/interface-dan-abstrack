79e0d6c66fa4b74b0a9dd5609f856c1a15a55ab1
VannyClaritaDewi <75290485+VannyClaritaDewi@users.noreply.github.com>
2021-05-23 21:56:02 +0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+    public static void main ( String []args){
+        Hewan garfield = new Kucing("Kucing", 4, "meongggggg- meeoong", "oranye");
+        Hewan kambs = new Kambing("Kambing", 4,"mbekkkk", "abu-abu");
+        garfield.tampilan();
+        kambs.tampilan();
+    }
+}
